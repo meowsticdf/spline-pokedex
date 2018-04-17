fabbbc055660511379680f9e70ffa7a4fea8e8f7
Andrew Ekstedt <ekstedta@oregonstate.edu>
2018-04-17 12:23:25 -0700

    merge spline setup.py

--- setup.py
+++ setup.py
@@ -1,28 +1,68 @@
 from setuptools import setup, find_packages
+
 setup(
     name = 'spline-pokedex',
     version = '0.1',
     packages = find_packages(),
 
     install_requires = [
-        'spline',
+        # spline
+        "Pylons>=1.0.1",
+        "SQLAlchemy>=0.6",
+        "Mako>=0.3.4",
+        "nose>=0.11",
+        "WTForms>=0.6",
+        'markdown',
+        'lxml',
+        'python-openid',
+        'webhelpers>=1.2',
+        # 'Babel>=0.9.5',  # needed for translation work only, can do without
+
+        # pokedex
         'pokedex',
         'SQLAlchemy>=0.7.5',
     ],
+    setup_requires=["PasteScript"],
 
     include_package_data = True,
-    package_data={'splinext': ['*/i18n/*/LC_MESSAGES/*.mo']},
+    package_data={
+        'spline': ['i18n/*/LC_MESSAGES/*.mo'],
+        'splinext': ['*/i18n/*/LC_MESSAGES/*.mo'],
+    },
 
     zip_safe = False,
+    test_suite='nose.collector',
 
-    entry_points = {'spline.plugins': [
-        'pokedex = splinext.pokedex:PokedexPlugin',
-    ]},
+    paster_plugins=['PasteScript', 'Pylons'],
+    entry_points="""
+    [paste.app_factory]
+    main = spline.config.middleware:make_app
+
+    [paste.app_install]
+    main = spline.installer:Installer
+
+    [babel.extractors]
+    spline-python = spline.babelplugin:extract_python
+    spline-mako = spline.babelplugin:extract_mako
+
+    [spline.plugins]
+    users = splinext.users:UsersPlugin
+    forum = splinext.forum:ForumPlugin
+    frontpage = splinext.frontpage:FrontPagePlugin
+    pokedex = splinext.pokedex:PokedexPlugin
+    """,
 
     namespace_packages = ['splinext'],
 
-    message_extractors = {'splinext': [
-        ('**.py', 'spline-python', None),
-        ('*/templates/**.mako', 'spline-mako', {'input_encoding': 'utf-8'}),
-        ('*/content/**.html', 'spline-mako', {'input_encoding': 'utf-8'})]},
+    message_extractors = {
+        'spline': [
+            ('**.py', 'spline-python', None),
+            ('**/templates/**.mako', 'spline-mako', {'input_encoding': 'utf-8'}),
+            ('**/public/**', 'ignore', None)],
+
+        'splinext': [
+            ('**.py', 'spline-python', None),
+            ('*/templates/**.mako', 'spline-mako', {'input_encoding': 'utf-8'}),
+            ('*/content/**.html', 'spline-mako', {'input_encoding': 'utf-8'})]
+    },
 )
