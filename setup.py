97181409b1c21a429e588816e702911228c65d67
Andrew Ekstedt <andrew.ekstedt@gmail.com>
2018-10-28 16:27:46 -0700

    add pyramid dependencies

--- setup.py
+++ setup.py
@@ -1,24 +1,49 @@
 from setuptools import setup, find_packages
 setup(
     name = 'spline-pokedex',
-    version = '0.1',
+    version = '0.2',
     packages = find_packages(),
 
     install_requires = [
-        'spline',
+        "pyramid>=1.5",
+        "pyramid_beaker",
+        "pyramid_debugtoolbar>=0.15.1",
+        "pyramid_mako>=1.0.2",
+        "pyramid_tm",
+        "Mako>=0.3.4",
+        "nose>=0.11",
+        "WTForms>=0.6,<2.0",
+        "markdown",
+        "lxml",
+        "webhelpers>=1.2",
+        "waitress>=1.1.0",
+        #"Babel>=0.9.5", # needed for translation work only, can do without
+
         'pokedex',
-        'SQLAlchemy>=0.7.5',
+        'SQLAlchemy>=0.7.5,<1.2.0b1',
+        'zope.sqlalchemy',
     ],
 
     include_package_data = True,
     package_data={'splinext': ['*/i18n/*/LC_MESSAGES/*.mo']},
 
     zip_safe = False,
+    test_suite='nose.collector',
 
-    entry_points = {'spline.plugins': [
-        'pokedex = splinext.pokedex:PokedexPlugin',
-        'gts = splinext.gts:GTSPlugin',
-    ]},
+    entry_points="""
+    [paste.app_factory]
+    main = splinext.pokedex.pyramidapp:main
+
+    [paste.app_install]
+    main = spline.installer:Installer
+
+    [babel.extractors]
+    spline-python = spline.babelplugin:extract_python
+    spline-mako = spline.babelplugin:extract_mako
+
+    #[nose.plugins]
+    #pylons = pylons.test:PylonsPlugin
+    """,
 
     namespace_packages = ['splinext'],
 
